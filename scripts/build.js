cfd5538f267b7e99674505deae28930226550733
Einar Nordfjord <einar@nordfjord.io>
2019-10-28 13:02:07 +0000

    more readable build output

--- scripts/build.js
+++ scripts/build.js
@@ -5,10 +5,13 @@ const cpuCount = os.cpus().length
 const spawnProcess = require('./utils/spawn-process')
 const packages = require('./utils/get-docker-projects')
 
+const max = (a, b) => Math.max(a, b)
+const nameLength = packages.map(x => x.dockerImageName.length).reduce(max, 0)
+
 Future.parallel(
   cpuCount,
   packages.map(info =>
-    spawnProcess(info.dockerImageName, 'docker', [
+    spawnProcess(info.dockerImageName.padEnd(nameLength, ' '), 'docker', [
       'build',
       info.path,
       '--tag',
--- scripts/utils/spawn-process.js
+++ scripts/utils/spawn-process.js
@@ -2,16 +2,52 @@ const Future = require('fluture')
 const chalk = require('chalk').default
 const { spawn } = require('child_process')
 
+let currentIdx = 0
+const colors = [
+  chalk.green,
+  chalk.cyan,
+  chalk.yellow,
+  chalk.magenta,
+  chalk.white,
+  chalk.gray,
+  chalk.blue,
+  chalk.greenBright,
+  chalk.yellowBright,
+  chalk.blueBright,
+  chalk.magentaBright,
+  chalk.cyanBright,
+  chalk.whiteBright,
+]
+
+const getColor = () => colors[currentIdx++ % colors.length]
+
 const spawnProcess = (name, command, spawnOptions) => {
   return Future((reject, resolve) => {
     const proc = spawn(command, spawnOptions)
 
+    const color = getColor()
+
+    const prefixName = s => `${color(`${name} |`)} ${s}`
+    const prefixError = s => `${chalk.red(`${name} |`)} ${s}`
+
     proc.stdout.on('data', data => {
-      console.log(`${chalk.cyan(name)} | ${data}`)
+      console.log(
+        data
+          .toString()
+          .split('\n')
+          .map(prefixName)
+          .join('\n')
+      )
     })
 
     proc.stderr.on('data', data => {
-      console.log(`${chalk.red(name)} | ${data}`)
+      console.error(
+        data
+          .toString()
+          .split('\n')
+          .map(prefixError)
+          .join('\n')
+      )
     })
 
     proc.on('close', code => {
